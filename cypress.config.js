4d52aa486ec231bff0be094a113eb8b4ab05a505
Marione-Tainara <mari.tainara@gmail.com>
2022-08-16 18:28:37 -0300

    adicionando informações do dashboard no arquivo de configurações

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "f2jtn4",
     baseUrl: "https://buger-eats-qa.vercel.app/",
     viewportWidth: 1440,
     viewportHeight: 900,
